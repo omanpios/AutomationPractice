6d5d912b39442f931d28bedc55686acb5e635055
Omar Pinilla Ossa <omar.pinillao@perficient.com>
2021-09-24 12:09:07 -0500

    Adjustments for crossbrowser testing across Chrome and Firefox

--- src/main/java/utils/BrowserManager.java
+++ src/main/java/utils/BrowserManager.java
@@ -1,9 +1,6 @@
 package utils;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -18,17 +15,13 @@ public class BrowserManager {
 
 	public WebDriver initializeDriver() throws IOException {
 
-		Properties prop = new Properties();
-		FileInputStream fis = new FileInputStream("src/main/java/utils/data.properties");
-		prop.load(fis);
-
-		String browserName = prop.getProperty("Browser");
+		String browserName = System.getProperty("Browser");
 
 		if (browserName.equals("Chrome")) {
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 		}
-		if (browserName.equals("Mozilla")) {
+		if (browserName.equals("Firefox")) {
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 		}
