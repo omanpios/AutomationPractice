8740310ef2142b1ee8c34fef1ea6ca29a833c56a
Omar Pinilla Ossa <omar.pinillao@perficient.com>
2021-09-30 14:11:03 -0500

    Adjustments to delete page objects classes and unify all the code within the test class

--- src/test/java/googleSearch/BuyABlouseTest.java
+++ src/test/java/googleSearch/BuyABlouseTest.java
@@ -1,6 +1,7 @@
 package googleSearch;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -13,6 +14,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -40,66 +43,59 @@ public class BuyABlouseTest extends BrowserManager {
     }
 
     @Test
-    @Order(1)
-    @DisplayName("Go to blouses category test")
-    void verifyGoToBlousesCategory() throws InterruptedException {
-        Actions actions = new Actions(driver);
-        IndexObjects objects = new IndexObjects(driver);
-        actions.moveToElement(objects.womenButton()).build().perform();
-        Thread.sleep(1000);
-        objects.blousesButton().click();
-        Assertions.assertTrue(objects.categoryName().equals("Blouses"));
-    }
-
-    @Test
-    @Order(2)
-    @DisplayName("Add item to cart and click checkout")
-    void verifyAddItemToCart() throws InterruptedException {
-        WebDriverWait wait = new WebDriverWait(driver, 5000);
-        Actions actions = new Actions(driver);
-        BlousesObjects objects = new BlousesObjects(driver);
-        actions.moveToElement(objects.productContainer());
-        objects.addToCartButton().click();
-        wait.until(ExpectedConditions.visibilityOf(objects.proceedToCheckOutButton()));
-        objects.proceedToCheckOutButton().click();
-        Assertions.assertTrue(objects.cartTitle().isDisplayed());
-        Thread.sleep(2000);
-    }
-
-    @Test
-    @Order(3)
-    @DisplayName("Checkout process (Summary, Sign in, Address and Shipping)")
-    void verifyCheckout() throws IOException, InterruptedException {
+    void verifyGoToBlousesCategory() throws InterruptedException, IOException {
         FileInputStream fis = new FileInputStream("src/main/java/utils/data.properties");
         prop.load(fis);
         String email = prop.getProperty("email");
         String password = prop.getProperty("pass");
-        CheckoutObjects cObjects = new CheckoutObjects(driver);
-        cObjects.checkoutButton().click();
-        cObjects.emailField().click();
-        cObjects.emailField().sendKeys(email);
-        cObjects.passwordField().click();
-        cObjects.passwordField().sendKeys(password);
-        cObjects.submitButton().click();
-        Thread.sleep(1000);
-        cObjects.processAdressButton().click();
-        cObjects.termsCheck().click();
-        cObjects.processCarrier().click();
-        Thread.sleep(1000);
-        Assertions.assertEquals("PLEASE CHOOSE YOUR PAYMENT METHOD", cObjects.header());
+        WebDriverWait wait = new WebDriverWait(driver, 5000);
+        Actions actions = new Actions(driver);
 
-    }
-
-    @Test
-    @Order(4)
-    @DisplayName("Verify if the order is completed")
-    void verifyCompletePayment() throws InterruptedException {
-        PaymentObjects pObjects = new PaymentObjects(driver);
-        pObjects.bankwireButton().click();
+        WebElement womenButton = driver.findElement(By.xpath("//*[@id='block_top_menu']/ul/li[1]/a"));
+        actions.moveToElement(womenButton).build().perform();
+        Thread.sleep(4000);
+        WebElement blousesButton = driver.findElement(By.linkText("Blouses"));
+        blousesButton.click();
+        Thread.sleep(4000);
+       
+        WebElement productContainer = driver.findElement(By.className("product-container"));
+        actions.moveToElement(productContainer);
+        
+        WebElement addToCartButton = driver.findElement(By.xpath("//span[contains(text(),'Add to cart')]"));
+        addToCartButton.click();
+        WebElement proceedToCheckOutButton = driver.findElement(By
+            .xpath("//body[1]/div[1]/div[1]/header[1]/div[3]/div[1]/div[1]/div[4]/div[1]/div[2]/div[4]/a[1]/span[1]"));
+        wait.until(ExpectedConditions.visibilityOf(proceedToCheckOutButton));
+        proceedToCheckOutButton.click();
         Thread.sleep(2000);
-        pObjects.confirmOrderButton().click();
+
+        WebElement checkoutButton = driver.findElement(By.partialLinkText("Proceed to checkout"));
+        checkoutButton.click();
+        WebElement emailField = driver.findElement(By.id("email"));
+        emailField.click();
+        emailField.sendKeys(email);
+        WebElement passwordField = driver.findElement(By.id("passwd"));
+        passwordField.click();
+        passwordField.sendKeys(password);
+        WebElement submitButton = driver.findElement(By.id("SubmitLogin"));
+        submitButton.click();
+        Thread.sleep(1000);
+        WebElement processAdressButton = driver.findElement(By.name("processAddress"));
+        processAdressButton.click();
+        WebElement termsCheck = driver.findElement(By.id("cgv"));
+        termsCheck.click();
+        WebElement processCarrier = driver.findElement(By.name("processCarrier"));
+        processCarrier.click();
+        Thread.sleep(1000);
+
+        WebElement bankwireButton = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[3]/div[1]/div/p/a"));
+        bankwireButton.click();
+        Thread.sleep(2000);
+        WebElement confirmOrderButton = driver.findElement(By.xpath("//*[@id='cart_navigation']/button"));
+        confirmOrderButton.click();
         Thread.sleep(3000);
-        Assertions.assertTrue(pObjects.confirmationMessage().equals("Your order on My Store is complete."));
+        String confirmationMessage = driver.findElement(By.xpath("//strong[contains(text(),'Your order on My Store is complete.')]")).getText();
+        Assertions.assertTrue(confirmationMessage.equals("Your order on My Store is complete."));
     }
 
     @AfterAll
